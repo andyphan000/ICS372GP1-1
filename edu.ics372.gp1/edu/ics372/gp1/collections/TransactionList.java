bd96e080f261fe5af24f1d3900f7a7553ad4c836
phana <phana@10.0.0.69>
2021-10-27 20:54:57 -0500

    testing

--- edu.ics372.gp1/edu/ics372/gp1/collections/TransactionList.java
+++ edu.ics372.gp1/edu/ics372/gp1/collections/TransactionList.java
@@ -6,21 +6,7 @@ import edu.ics372.gp1.Transaction;
 
 public class TransactionList {
 	private List<Transaction> transactions;
-	private Iterator transactionIterator;
+	
 
-	private TransactionList() {
-
-	}
-
-	public static TransactionList getInstance() {
-
-	}
-
-	public Iterator getTransactions() {
-		return this.transactionIterator;
-	}
-
-	public boolean insertTransaction() {
-
-	}
+	
 }
